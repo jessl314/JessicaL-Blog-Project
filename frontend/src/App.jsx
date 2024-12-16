1618f80594d601730e55f07acf16468bda5b623a
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-15 17:02:24 -0800

    Revert "added home, about me, project pages"
    This reverts commit 940dfff09494256f54b4d75cdf4a3ba1a23674d8.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,35 @@
-// import { useState } from 'react';
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { StrictMode } from 'react'
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Grin from "./pages/Grin";
-import Speepy from "./pages/Speepy";
-import NavBar from './Navbar.jsx';
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <StrictMode>
-      <NavBar />
-      <h1>App</h1>
-    </StrictMode>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App

900735972e71a38a0817bb5487ac36b2a3776a19
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-16 11:55:21 -0800

    didn't save resume location changeoops

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card'
 import JessicaImage from '../assets/JessicaHeadShot.jpg'
-import Resume from '../assets/Liao,Jessica_Resume.pdf'
 
 function About () {
     return (
@@ -16,7 +15,7 @@ function About () {
                 </Card.Text>
             </Card.Body>
             <Card.Body>
-                <Card.Link href={Resume} target="_blank" rel="noopener noreferrer">View Resume</Card.Link>
+                <Card.Link href="/Liao,Jessica_Resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</Card.Link>
             </Card.Body>
         </Card>
     )

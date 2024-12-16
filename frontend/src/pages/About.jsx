e6c2008a2b9d738be578337966a164f6eeb9b968
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-16 11:47:53 -0800

    changed the card to the correct page and fixed resume link

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import JessicaImage from '../assets/JessicaHeadShot.jpg'
+import Resume from '../assets/Liao,Jessica_Resume.pdf'
 
 function About () {
     return (
-        <h1>About</h1>
+        // <img src={JessicaImage} alt="A headshot of Jessica wearing a red jacket and a yellow shirt." className="img-fluid thumbnail"/>
+        <Card style={{ width: '18rem'}}>
+            <Card.Img variant="top" src={JessicaImage} />
+            <Card.Body>
+                <Card.Title>Jessica Liao</Card.Title>
+                <Card.Text>
+                    Click below to view my current resume and learn more about my professional journey!
+                </Card.Text>
+            </Card.Body>
+            <Card.Body>
+                <Card.Link href={Resume} target="_blank" rel="noopener noreferrer">View Resume</Card.Link>
+            </Card.Body>
+        </Card>
     )
 
 }

67236c5320e762057a22cf7eabb2c593938e25ef
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-16 11:27:32 -0800

    added my headshot and card component
    this will be an image of me along with a link to my resume. I'm planning on putting a elevator pitch next to this and then links to my socials + github below

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import JessicaImage from './assets/JessicaHeadShot.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Home () {
     return (
-        <h1>Home</h1>
+        // <img src={JessicaImage} alt="A headshot of Jessica wearing a red jacket and a yellow shirt." className="img-fluid thumbnail"/>
+        <Card style={{ width: '18rem'}}>
+            <Card.Img variant="top" src={JessicaImage} />
+            <Card.Body>
+                <Card.Title>Jessica Liao</Card.Title>
+                <Card.Text>
+                    Click below to view my current resume and learn more about my professional journey!
+                </Card.Text>
+            </Card.Body>
+            <Card.Body>
+                <Card.Link href="/assets/Liao,Jessica_Resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</Card.Link>
+            </Card.Body>
+        </Card>
     )
 
 }

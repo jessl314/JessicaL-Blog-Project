a8ed0e77fc34fb9e5116bc95e7781f76aa26eb11
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-17 12:00:16 -0800

    changed positioning of buttons

--- frontend/src/pages/AboutSocial.jsx
+++ frontend/src/pages/AboutSocial.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col, Button } from 'react-bootstrap';
 import { FaInstagram, FaGithub, FaLinkedin} from "react-icons/fa";
+import './About.css'
 
 const SocialButtons = () => {
     return (
-        <Row className="justify-content-end mt-4 me-3">
+        <Row className="justify-content-end mt-4 me-2 social container">
             <Col xs={4} md={2} className="d-flex justify-content-center mb-2">
                 <a href ="https://www.instagram.com/chamomiletea314/" target="_blank" rel="noopener noreferrer">
                     <Button variant="light" className="rounded-circle">

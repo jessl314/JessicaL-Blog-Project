32d0e2d6d34cba50101b453075c514e6a3a62bfa
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-17 09:02:01 -0800

    added icons and links for my socials

---
+++ frontend/src/pages/AboutSocial.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaInstagram, FaGithub, FaLinkedin} from "react-icons/fa";
+
+
+
+
+// <FaInstagram />
+// <FaGithub />
+// <FaLinkedin />
+// https://github.com/jessl314
+// https://www.linkedin.com/in/jessica-liao-089359244
+// https://www.instagram.com/chamomiletea314/ 

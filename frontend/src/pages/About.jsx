da2eebb6baf2867be49d06662f4526410f3ce5a0
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-18 08:01:25 -0800

    got rid of some css due to requirements + got rid of resume

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -20,14 +20,11 @@ function About () {
                     Click below to view my current resume and learn more about my professional journey!
                 </Card.Text>
               </Card.Body>
-              <Card.Body>
-                <Card.Link href="/Liao,Jessica_Resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</Card.Link>
-              </Card.Body>
             </Card>
           </Col>
           {/* my elevator pitch/about me */}
-          <Col xs={12} md={6} className="about-me">
-          <h2 className="about-me-title">About Me</h2>
+          <Col xs={12} md={6}>
+          <h2>About Me</h2>
           <p>Hello World! My name is Jessica Liao and I am a current second-year Computer Science Major at UCI. I love to learn and have become proficient in Python, C++ and most recently React! Outside of classes, I have collaborated on a project in Video Game Development Club, I am a Logistics Organizer for Venushacks and so much more! </p>
           <p>For fun, I like to read, watch tv, crochet and try new foods (I also really like boba!) </p>
           </Col>

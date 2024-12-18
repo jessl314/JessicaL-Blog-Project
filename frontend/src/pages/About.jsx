e6f84106af6890e6f919a9e7f1a9761c2887ee65
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-18 09:48:20 -0800

    added some clarifying comments

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,21 +8,22 @@ import "./About.scss"
 
 function About () {
     return (
-    // using Bootstrap Grid system to align my picture card and about me in one row and my social media/github in another
+    // uses Bootstrip Grid system
     <Container className="mt-5">
       <Row className="align-items-start justify-content-center">
+          {/* this column is for my picture which is in a oval shaped image. It will be left of the center in web-view */}
           <Col xs={12} md={6} className="d-flex justify-content-center mb-4">
            {/* card component, contains my picture and a link to my resume */}
            <Image roundedCircle fluid src={JessicaImage} alt="me" width="60%" height = "60%"/>
           </Col>
-          {/* my elevator pitch/about me */}
+          {/* my elevator pitch/about me. it will be right of center in web-view and stacked under my picture in mobile view*/}
           <Col xs={12} md={6}>
           <h2 className="mb-4">About Me</h2>
           <p>Hello World! My name is Jessica Liao and I am a current second-year Computer Science Major at UCI. I love to learn and have become proficient in Python, C++ and most recently React! Outside of classes, I have collaborated on a project in Video Game Development Club, I am a Logistics Organizer for Venushacks and so much more! </p>
           <p>For fun, I like to read, watch tv, crochet and try new foods (I also really like boba!) </p>
           <SocialButtons/>
+           {/* this component (from ./AboutSocial) will be under my elevator pitch/about me. all three buttons will be horizontally next to each other and as a unit will be stacked under my elevator pitch in mobile-view */}
           </Col>
-          
       </Row>
     </Container>
     )
--- frontend/src/pages/AboutSocial.jsx
+++ frontend/src/pages/AboutSocial.jsx
@@ -7,16 +7,22 @@ import './About.scss'
 const SocialButtons = () => {
     return (
         <>
+        {/* for the About Me page */}
+        {/* for all buttons, when clicked, the corresponding profile page should show up in a new tab and when hovering over the button, the website name should show up */}
+
+        {/* Instagram button */}
             <a href ="https://www.instagram.com/chamomiletea314/" target="_blank" rel="noopener noreferrer" title="Instagram">
                 <Button variant="light" className="rounded-circle social-button">
                 <FaInstagram size={64} color="black"/>
                 </Button>
             </a>
+        {/* Linkedin button */}
             <a href ="https://www.linkedin.com/in/jessica-liao-089359244" target="_blank" rel="noopener noreferrer" title="Linkedin">
                 <Button variant="light" className="rounded-circle social-button">
                 <FaLinkedin size={64} color="black"/>
                 </Button>
             </a>
+        {/* Github button */}
             <a href ="https://github.com/jessl314" target="_blank" rel="noopener noreferrer" title="Github">
                 <Button variant="light" className="rounded-circle social-button">
                 <FaGithub size={64} color="black"/>

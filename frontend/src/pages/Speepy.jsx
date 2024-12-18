30e147d058a93a12b6dc1896bf19a56a2bbb7952
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-18 13:34:07 -0800

    added my vdgc project link

--- frontend/src/pages/Speepy.jsx
+++ frontend/src/pages/Speepy.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Button } from "react-bootstrap"
 import "./Speepy.scss"
 
 function Speepy () {
@@ -8,7 +8,11 @@ function Speepy () {
         <Container className="mt-5">
             <Row className="align-items-start justify-content-start">
                 <Col xs={8} md={4} className="d-flex justify-content-start mb-4 ms-0">
-                <h1 className="speepy-title">Speepy</h1>
+                    <h1 className="speepy-title">Speepy</h1>
+                    <p> Check out <a href="https://itch.io/jam/vgdc-at-uci-fall-2024-games/rate/3151721" target="_blank" rel="noopener noreferrer" className="styled-link">hackathon submission</a> for XYZ Hackathon.
+</p>
+                   
+                    
                 </Col>
             </Row>
             
@@ -18,3 +22,5 @@ function Speepy () {
 }
 
 export default Speepy
+
+// "https://itch.io/jam/vgdc-at-uci-fall-2024-games/rate/3151721"

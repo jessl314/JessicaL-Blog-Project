a8ccd1b35a21cc8e7d289f2e30560d2da144579f
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-19 14:20:47 -0800

    switched to pixabay api

--- backend/server.js
+++ backend/server.js
@@ -7,37 +7,28 @@ const port = 5000;
 
 app.use(cors());
 
-const pexelsAPIKey = 'eLaNYZUymi9Rxf39jlIChTmvokyrbZENPyQEbQKPmRhTaIhwYYfRjrHb';
+const pixaBayAPIKey = '47745339-b5ac535ed90ab426c13aaa83c';
 
 // res: response object
 app.get('/api/cat-image', async (req, res) => {
     try {
         const query = 'Cats';
         const response = await axios.get('https://api.pexels.com/v1/search', {
-            headers: {
-                Authorization: `Bearer ${pexelsAPIKey}`
-            },
             params: {
-                query,
+                key: pixaBayAPIKey,
+                q: query,
+                image_type: 'photo',
                 per_page: 1,
             }
         });
 
-        if (response.data.photos.length > 0) {
-            // the data response that we get
-            res.json({
-                imageURL: response.data.photos[0].src.original,
-                imagePhotographer: response.data.photos[0].photographer
-            });
-            console.log({imageURL})
-        } else {
-            // resource not found
-            res.status(404).json({ error: 'No images found'});
-        }
+        const imageURL = response.data.hits[0].webformatURL;
+        const imagePhotographer = response.data.hits[0].user;
+        res.json({imageURL, imagePhotographer})
     } catch (error) {
         console.error('Error fetching cat image: ', error);
         // internal server error
-        res.status(500).json({ error: 'Error fetching data from Pexels'});
+        res.status(500).json({ error: 'Error fetching cat image'});
     }
 });
 

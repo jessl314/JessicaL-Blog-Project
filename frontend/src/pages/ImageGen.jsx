28220362655cc1a3031a1b318263039320ff4765
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-19 09:32:38 -0800

    documentation comments for image generation function

--- frontend/src/pages/ImageGen.jsx
+++ frontend/src/pages/ImageGen.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 
 function ImageGen () {
     const [showImage, setShowImage] = useState(false);
-
+    // when button is clicked, showImage = true, meaning that we should show image
     const handleButtonClick = () => {
         setShowImage(!showImage);
     };
@@ -16,9 +16,13 @@ function ImageGen () {
        <>
        <h1>cat image generator</h1>
 
+       {/* this is the button. when it is clicked we will use the handleButtonClick event handler */}
        <Button variant="light" className="rounded-circle" onClick={handleButtonClick}>Show Cat!</Button>
+
+       {/* if showImage is true, then the DisplayImage component will be executed otherwise it won't */}
        {showImage && <DisplayImage />}
        
+       {/* credit to Pexels */}
        <a href="https://www.pexels.com">Photos provided by Pexels</a>
 
        </>

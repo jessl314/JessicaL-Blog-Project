79caba37d5faf3c4d830518ed88590aaeca4c934
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-29 19:14:41 -0800

    I attempted to fix styling errors

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,24 +2,33 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/Home.scss";
 import {Link} from "react-router-dom";
+import {Row, Col, Container} from "react-bootstrap";
 
 function Home () {
     return (
-       <div>
+        <Container>
+        <div>
         {/* get rid of Hello World on my about me page */}
         <h1>Welcome to Jessica's Corner!</h1>
         <h5>Here are some quick access links!</h5>
-        <ol className="p-3 rounded intro-block">
+        <ul className="p-3 rounded intro-block">
         <li><Link to="/about">About Me</Link></li>
         <li><Link to="/DisplayImage">Cat!</Link></li>
-        <p>Projects!</p>
+        </ul>
+        <div>
+        <h6>Projects!</h6>
+        </div>
+        <ul className="p-3 rounded intro-block">
         <li><Link to="/Speepy">Speepy</Link></li>
         <li><Link to="/Solace">Solace</Link></li>
-        </ol>
+        </ul>
 
         {/* fun facts?? a most recent achievement box?? */}
 
        </div>
+
+        </Container>
+      
     )
 
 }

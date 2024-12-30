123797fba0148e9b3e34d05e14a2f220fb4238d5
jessl314 <jlchrysanthemum456@gmail.com>
2024-12-29 19:35:22 -0800

    made my home page a little better

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,25 +7,32 @@ import {Row, Col, Container} from "react-bootstrap";
 function Home () {
     return (
         <Container>
-        <div>
-        {/* get rid of Hello World on my about me page */}
-        <h1>Welcome to Jessica's Corner!</h1>
-        <h5>Here are some quick access links!</h5>
-        <ul className="p-3 rounded intro-block">
-        <li><Link to="/about">About Me</Link></li>
-        <li><Link to="/DisplayImage">Cat!</Link></li>
-        </ul>
-        <div>
-        <h6>Projects!</h6>
-        </div>
-        <ul className="p-3 rounded intro-block">
-        <li><Link to="/Speepy">Speepy</Link></li>
-        <li><Link to="/Solace">Solace</Link></li>
-        </ul>
+             <Row className="justify-content-center">
+                <Col xs={8} md={6} className="d-flex justify-content-center mb-4 ms-0">
+                <div>
+                {/* get rid of Hello World on my about me page */}
+                <h1>Welcome to Jessica's Corner!</h1>
+                <h5>Here are some quick access links!</h5>
+                <ul className="p-3 rounded intro-block">
+                <li><Link to="/about">About Me</Link></li>
+                <li><Link to="/DisplayImage">Cat!</Link></li>
+                </ul>
+                <div>
+                <h6>Projects!</h6>
+                </div>
+                <ul className="p-3 rounded intro-block">
+                <li><Link to="/Speepy">Speepy</Link></li>
+                <li><Link to="/Solace">Solace</Link></li>
+                 </ul>
 
         {/* fun facts?? a most recent achievement box?? */}
 
-       </div>
+            </div>
+                </Col>
+
+
+             </Row>
+        
 
         </Container>
       
